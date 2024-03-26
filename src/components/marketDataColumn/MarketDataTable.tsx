7a69254751ad29ab54f88eb288fc0f0925b7713f
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2024-03-26 13:59:08 +0500

    таблица для отрисовки курса валют

---
+++ src/components/marketDataColumn/MarketDataTable.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import calculateLowestRates from "../../helpers/calculateLowestRates.helper.ts";
+import { IRates } from "../../interfaces/rates.interface.ts";
+import RateCell from "./rateCell/RateCell.tsx";
+import "./MarketDataColumn.css";
+import { ICurrencyRate } from "../../interfaces/currencyRate.interface.ts";
+
+interface MarketDataColumnProps {
+  data: ICurrencyRate[];
+}
+
+const MarketDataTable: React.FC<MarketDataColumnProps> = ({ data }) => {
+  const [firstRates, secondRates, thirdRates] = data;
+  const lowestRates = calculateLowestRates(
+    firstRates.rates,
+    secondRates.rates,
+    thirdRates.rates,
+  );
+  const currencies = Object.keys(firstRates.rates) as (keyof IRates)[];
+
+  return (
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">First</th>
+            <th scope="col">Second</th>
+            <th scope="col">Third</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currencies.map((currency) => (
+            <tr key={currency}>
+              <td>{currency}/CUPCAKE</td>
+              <RateCell
+                rate={firstRates.rates[currency]}
+                lowestRate={lowestRates[currency]}
+              />
+              <RateCell
+                rate={secondRates.rates[currency]}
+                lowestRate={lowestRates[currency]}
+              />
+              <RateCell
+                rate={thirdRates.rates[currency]}
+                lowestRate={lowestRates[currency]}
+              />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MarketDataTable;
